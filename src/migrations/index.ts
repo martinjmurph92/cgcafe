a445341a2269ba4ecf77c20a85329e7caa28ce22
Martin Murphy <martin@fluid-ideas.co.uk>
2026-03-13 06:24:28 +0000

    Added migrations locally.

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260313_062337 from './20260313_062337';
+
+export const migrations = [
+  {
+    up: migration_20260313_062337.up,
+    down: migration_20260313_062337.down,
+    name: '20260313_062337'
+  },
+];
